2517ef50ca300e5e5e668482544610833b6a9407
Linwei Zhang <lewiszlw520@gmail.com>
2024-11-15 14:39:44 +0800

    Complete without alignment

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
+use core::ptr::NonNull;
+
+use allocator::{AllocError, BaseAllocator, ByteAllocator, PageAllocator};
 
 /// Early memory allocator
 /// Use it before formal bytes-allocator and pages-allocator can work!
@@ -16,16 +18,99 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator;
-
-impl EarlyAllocator {
+pub struct EarlyAllocator<const PAGE_SIZE: usize> {
+    start: usize,
+    end: usize,
+    b_pos: usize,
+    p_pos: usize,
 }
 
-impl BaseAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> EarlyAllocator<PAGE_SIZE> {
+    pub const fn new() -> Self {
+        EarlyAllocator {
+            start: 0,
+            end: 0,
+            b_pos: 0,
+            p_pos: 0,
+        }
+    }
 }
 
-impl ByteAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> BaseAllocator for EarlyAllocator<PAGE_SIZE> {
+    fn init(&mut self, start: usize, size: usize) {
+        assert!(PAGE_SIZE.is_power_of_two());
+        self.start = start;
+        self.end = start + size;
+        self.b_pos = start;
+        self.p_pos = start + size;
+    }
+
+    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
+        Err(AllocError::NoMemory) // unsupported
+    }
 }
 
-impl PageAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> ByteAllocator for EarlyAllocator<PAGE_SIZE> {
+    fn alloc(&mut self, layout: core::alloc::Layout) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
+        let start = self.b_pos;
+        let aligned_start = (start + layout.align() - 1) & !(layout.align() - 1);
+        assert!(aligned_start >= start);
+
+        if aligned_start + layout.size() <= self.p_pos {
+            self.b_pos = aligned_start + layout.size();
+            unsafe {
+                Ok(NonNull::new_unchecked(aligned_start as *mut u8))
+            }
+        } else {
+            Err(AllocError::NoMemory)
+        }
+    }
+
+    fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
+        // do nothing
+    }
+
+    fn total_bytes(&self) -> usize {
+        self.end - self.start
+    }
+
+    fn used_bytes(&self) -> usize {
+        (self.b_pos - self.start) + (self.end - self.p_pos)
+    }
+
+    fn available_bytes(&self) -> usize {
+        self.p_pos - self.b_pos
+    }
+}
+
+impl<const PAGE_SIZE: usize> PageAllocator for EarlyAllocator<PAGE_SIZE> {
+    const PAGE_SIZE: usize = PAGE_SIZE;
+
+    fn alloc_pages(&mut self, num_pages: usize, align_pow2: usize) -> allocator::AllocResult<usize> {
+        let size = num_pages * PAGE_SIZE;
+        // TODO alignment
+        let aligned_start = self.p_pos - size;
+        if aligned_start >= self.b_pos {
+            self.p_pos = aligned_start;
+            Ok(aligned_start)
+        } else {
+            Err(AllocError::NoMemory)
+        }
+    }
+
+    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
+        // do nothing
+    }
+
+    fn total_pages(&self) -> usize {
+        (self.end - self.start ) / PAGE_SIZE
+    }
+
+    fn used_pages(&self) -> usize {
+        (self.end - self.p_pos ) / PAGE_SIZE
+    }
+
+    fn available_pages(&self) -> usize {
+        (self.p_pos - self.b_pos ) / PAGE_SIZE
+    }
 }
